2b8b2aee043c238261420af9fa396102aca9a67e
Rafael Rubem Rossi Souza Guerreiro <guerreiro.dev@gmail.com>
2016-07-01 23:08:08 -0300

    comment undefined method

--- config/puma.rb
+++ config/puma.rb
@@ -4,3 +4,4 @@ cwd = File.dirname(__FILE__) + '/..'
 
 ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
 ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || YAML.load_file("#{cwd}/config/database.yml")[ENV['RACK_ENV']])
+# ActiveRecord::Base.verify_active_connections!

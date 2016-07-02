746d9eb461383ae8b3bc900073956abff5a0ea6d
Rafael Rubem Rossi Souza Guerreiro <guerreiro.dev@gmail.com>
2016-07-02 15:35:23 -0300

    Add spec for ApplicationController

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe ApplicationController, type: :controller do
+  it 'prevents CSRF attacks with exception' do
+    strategy = ActionController::RequestForgeryProtection::ProtectionMethods::Exception
+    expect(subject.forgery_protection_strategy).to eq(strategy)
+  end
+end

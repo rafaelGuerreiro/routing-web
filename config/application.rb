0b5de5471cb53c57120831f97e07ba937f95c61f
Rafael Rubem Rossi Souza Guerreiro <guerreiro.dev@gmail.com>
2016-07-01 23:23:53 -0300

    Move bg to a separate erb file

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module RoutingWeb
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.secret_key_base = Figaro.env.secret_key_base
+    config.serve_static_assets = true
   end
 end

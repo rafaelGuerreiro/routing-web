efc010e503fce1c66e96d28dcb91cacc3128d941
Rafael Rubem Rossi Souza Guerreiro <guerreiro.dev@gmail.com>
2016-07-01 22:47:29 -0300

    Add devise views

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,34 @@
+module DeviseHelper
+  def devise_error_messages!
+    return '' unless devise_error_messages?
+
+    html_error_explanation
+  end
+
+  def devise_error_messages?
+    !resource.errors.empty?
+  end
+
+  private
+
+  def html_error_explanation
+    html = <<-HTML
+    <div id="error_explanation" class="alert alert-danger">
+      <p class="margin-0"><strong>#{sentence}</strong></p>
+      <ul>#{messages}</ul>
+    </div>
+    HTML
+
+    html.html_safe
+  end
+
+  def sentence
+    I18n.t('errors.messages.not_saved',
+           count: resource.errors.count,
+           resource: resource.class.model_name.human.downcase)
+  end
+
+  def messages
+    resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+  end
+end

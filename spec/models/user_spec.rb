456c1400cb3e6471ee9c72b34b01f633b07b2134
Rafael Rubem Rossi Souza Guerreiro <guerreiro.dev@gmail.com>
2016-07-02 21:52:36 -0300

    Improve readability of user's test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,26 +1,26 @@
 require 'rails_helper'
 
 describe User, type: :model do
-  context 'factory' do
+  describe 'factory' do
     it 'has a valid factory' do
       expect(build(:user)).to be_valid
     end
   end
 
-  context 'validations' do
+  describe 'validations' do
     before { create(:user) }
 
-    context 'presence' do
-      it { should validate_presence_of :email }
-      it { should validate_presence_of :encrypted_password }
+    describe 'presence' do
+      it { is_expected.to validate_presence_of :email }
+      it { is_expected.to validate_presence_of :encrypted_password }
     end
 
-    context 'uniqueness' do
-      it { should validate_uniqueness_of(:email).case_insensitive }
+    describe 'uniqueness' do
+      it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
     end
   end
 
-  context 'devise' do
+  describe 'devise modules' do
     let(:modules) do
       [
         :database_authenticatable, :registerable, :confirmable,

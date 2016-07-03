4f4db17b5a84cddd256007ac9d7fbe55b5f04040
Rafael Rubem Rossi Souza Guerreiro <guerreiro.dev@gmail.com>
2016-07-03 15:31:23 -0300

    Add SendGrid smtp configuration

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  address: 'smtp.sendgrid.net',
+  port: '587',
+  authentication: :plain,
+  user_name: Figaro.env.sendgrid_username,
+  password: Figaro.env.sendgrid_password,
+  domain: 'heroku.com',
+  enable_starttls_auto: true
+}

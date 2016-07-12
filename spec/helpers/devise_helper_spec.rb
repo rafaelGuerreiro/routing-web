e7117c02d268bb987517dca18d970c29cda98dd8
Rafael Rubem Rossi Souza Guerreiro <guerreiro.dev@gmail.com>
2016-07-12 17:42:23 -0300

    Improve test for DeviseHelper

--- spec/helpers/devise_helper_spec.rb
+++ spec/helpers/devise_helper_spec.rb
@@ -9,29 +9,26 @@
 #   end
 # end
 describe DeviseHelper, type: :helper do
-  let(:dummy_resource) do
-    Class.new do
-      attr_reader :errors
+  class DummyResource
+    attr_reader :errors
 
-      def initialize(errors = [])
-        @errors = errors
-      end
+    def initialize(errors = [])
+      @errors = errors
     end
   end
 
-  let(:dummy_resource_with_errors) { dummy_resource.new(['error', 'another error']) }
-  let(:dummy_resource_without_errors) { dummy_resource.new }
+  let(:dummy_resource) { DummyResource.new(['error', 'another error']) }
+  let(:dummy_resource_without_errors) { DummyResource.new }
 
   context 'when there are errors' do
-    before(:each) do
+    before do
+      dummy = dummy_resource
       helper.class.__send__(:define_method, :resource) do
-        return dummy_resource_with_errors
+        return dummy
       end
-      # allow(helper).to receive(:resource).and_return(Resource.new)
     end
 
     describe '#devise_error_messages!' do
-      it { expect(helper.devise_error_messages?).to be_truthy }
     end
 
     describe '#devise_error_messages?' do
@@ -40,12 +37,18 @@ describe DeviseHelper, type: :helper do
   end
 
   context 'when there are no errors' do
-    before(:each) do
+    before do
+      dummy = dummy_resource_without_errors
       helper.class.__send__(:define_method, :resource) do
-        return dummy_resource_without_errors
+        return dummy
       end
     end
 
-    # its (:devise_error_messages?) { is_expected.to be_falsy }
+    describe '#devise_error_messages!' do
+    end
+
+    describe '#devise_error_messages?' do
+      it { expect(helper.devise_error_messages?).to be_falsy }
+    end
   end
 end

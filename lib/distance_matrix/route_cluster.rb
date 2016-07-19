964800b7cb715ca066b2955fbbc6bd03f015d77a
Rafael Rubem Rossi Souza Guerreiro <guerreiro.dev@gmail.com>
2016-07-19 19:22:15 -0300

    Add routes cluster to group routes and process them all together

---
+++ lib/distance_matrix/route_cluster.rb
@@ -0,0 +1,80 @@
+require 'set'
+
+module DistanceMatrix
+  class RouteCluster
+    include ActiveModel::Validations
+    include Hashie
+    include Dirtie
+    include PrettyStringfy
+
+    attr_reader :origin
+
+    validates :origin, presence: true, location: true
+
+    def initialize(origin:, routes: [])
+      @origin = origin
+      @routes = Set.new
+      routes.each { |d| self << d }
+    end
+
+    def <<(route)
+      return unless valid_route?(route)
+
+      dirty! if @routes.add?(route)
+    end
+    alias add <<
+
+    def size
+      @routes.size
+    end
+    alias length size
+
+    def to_cluster
+      return @cluster if clean?
+
+      @cluster = []
+
+      @routes.each_slice(100) do |slice|
+        @cluster << to_cluster_hash(slice)
+      end
+
+      clean!
+      @cluster.freeze
+    end
+
+    def routes
+      @routes.to_a
+    end
+
+    def each
+      return to_cluster unless block_given?
+
+      to_cluster.each do |hash|
+        yield hash
+      end
+    end
+
+    private
+
+    def valid_route?(route)
+      route.is_a?(DistanceMatrix::Route) && route.valid? && route.origin == @origin
+    end
+
+    def to_cluster_hash(routes)
+      {
+        origin: @origin,
+        routes: routes,
+        query_parameter: to_query_parameter(routes)
+      }
+    end
+
+    def to_query_parameter(routes)
+      return '' unless routes.present?
+      "origins=#{@origin.to_query_parameter}&destinations=#{to_query_parameters(routes)}"
+    end
+
+    def to_query_parameters(routes)
+      routes.map(&:destination).map(&:to_query_parameter).join('|')
+    end
+  end
+end
---
+++ spec/lib/distance_matrix/route_cluster_spec.rb
@@ -0,0 +1,166 @@
+require 'set'
+
+describe DistanceMatrix::RouteCluster do
+  describe 'modules' do
+    subject { DistanceMatrix::RouteCluster }
+
+    it { is_expected.to include(Hashie) }
+    it { is_expected.to include(PrettyStringfy) }
+    it { is_expected.to include(ActiveModel::Validations) }
+  end
+
+  let(:sp) { DistanceMatrix::Location.new(city: 'SAO PAULO', state: 'SP') }
+  let(:rs) { DistanceMatrix::Location.new(city: 'PORTO ALEGRE', state: 'RS') }
+  let(:rj) { DistanceMatrix::Location.new(city: 'RIO DE JANEIRO', state: 'RJ') }
+  let(:sc) { DistanceMatrix::Location.new(city: 'FLORIANOPOLIS', state: 'SC') }
+
+  let(:invalid_sp) { DistanceMatrix::Location.new(city: nil, state: 'SP') }
+
+  let(:sp_rs) { DistanceMatrix::Route.new(origin: sp, destination: rs) }
+  let(:sp_rj) { DistanceMatrix::Route.new(origin: sp, destination: rj) }
+  let(:sp_sc) { DistanceMatrix::Route.new(origin: sp, destination: sc) }
+
+  let(:invalid_sp_rs) { DistanceMatrix::Route.new(origin: invalid_sp, destination: rs) }
+
+  let(:rs_sp) { DistanceMatrix::Route.new(origin: rs, destination: sp) }
+  let(:rs_rj) { DistanceMatrix::Route.new(origin: rs, destination: sp) }
+
+  let(:invalid_rs_sp) { DistanceMatrix::Route.new(origin: rs, destination: invalid_sp) }
+
+  describe '.new' do
+    it 'adds every route when there are any' do
+      routes = [sp_rs, sp_rj, sp_sc]
+      cluster = DistanceMatrix::RouteCluster.new(origin: sp, routes: routes)
+
+      expect(cluster.routes).to contain_exactly(*routes)
+    end
+  end
+
+  describe '#<<' do
+    context 'when origin is valid' do
+      subject { DistanceMatrix::RouteCluster.new(origin: sp) }
+
+      context 'when added route is valid' do
+        context 'when added route is from the same origin as the cluster' do
+          it { expect { subject << sp_rs }.to change { subject.routes }.from([]).to([sp_rs]) }
+          it { expect { subject << sp_rj }.to change { subject.routes }.from([]).to([sp_rj]) }
+        end
+
+        context 'when added route is from another origin as of the cluster' do
+          it { expect { subject << rs_sp }.to_not change { subject.routes } }
+          it { expect { subject << rs_rj }.to_not change { subject.routes } }
+        end
+      end
+
+      context 'when added route is invalid' do
+        context 'when added object is a DistanceMatrix::Route' do
+          context 'when added route is from the same origin as the cluster' do
+            it { expect { subject << invalid_sp_rs }.to_not change { subject.routes } }
+          end
+
+          context 'when added route is from another origin as of the cluster' do
+            it { expect { subject << invalid_rs_sp }.to_not change { subject.routes } }
+          end
+        end
+
+        context 'when added object is not a DistanceMatrix::Route' do
+          it { expect { subject << nil }.to_not change { subject.routes } }
+          it { expect { subject << 'from SP to RJ' }.to_not change { subject.routes } }
+          it { expect { subject << 123 }.to_not change { subject.routes } }
+        end
+      end
+    end
+
+    context 'when origin is invalid' do
+      subject { DistanceMatrix::RouteCluster.new(origin: invalid_sp) }
+
+      context 'when added route is valid' do
+        it { expect { subject << sp_rs }.to_not change { subject.routes } }
+        it { expect { subject << sp_rj }.to_not change { subject.routes } }
+      end
+
+      context 'when added route is invalid' do
+        context 'when added object is a DistanceMatrix::Route' do
+          context 'when added route is from the same origin as the cluster' do
+            it { expect { subject << invalid_sp_rs }.to_not change { subject.routes } }
+          end
+
+          context 'when added route is from another origin as of the cluster' do
+            it { expect { subject << invalid_rs_sp }.to_not change { subject.routes } }
+          end
+        end
+
+        context 'when added object is not a DistanceMatrix::Route' do
+          it { expect { subject << nil }.to_not change { subject.routes } }
+          it { expect { subject << 'from SP to RJ' }.to_not change { subject.routes } }
+          it { expect { subject << 123 }.to_not change { subject.routes } }
+        end
+      end
+    end
+  end
+
+  describe '#size' do
+    subject { DistanceMatrix::RouteCluster.new(origin: sp) }
+
+    context 'when route is added' do
+      it { expect { subject << sp_rj }.to change { subject.size }.by(1) }
+    end
+
+    context 'when route is not added' do
+      it { expect { subject << nil }.to_not change { subject.size } }
+    end
+  end
+
+  describe '#routes' do
+    context 'when there are routes' do
+      subject { DistanceMatrix::RouteCluster.new(origin: sp, routes: [sp_rj]) }
+
+      it 'returns an Array' do
+        expect(subject.routes).to be_an(Array)
+      end
+
+      it 'does not allow the original route to be modified' do
+        expect { subject.routes << 'an string' }.to_not change { subject.routes }
+      end
+    end
+
+    context 'when there are no routes' do
+      subject { DistanceMatrix::RouteCluster.new(origin: sp) }
+
+      it 'returns an empty Array' do
+        expect(subject.routes).to eql([])
+      end
+
+      it 'does not allow the original route to be modified' do
+        expect { subject.routes << 'an string' }.to_not change { subject.routes }
+      end
+    end
+  end
+
+  describe '#to_cluster' do
+    context 'when there are routes' do
+      subject { DistanceMatrix::RouteCluster.new(origin: sp, routes: [sp_rs, sp_rj]) }
+
+      it 'returns a frozen Array' do
+        cluster = subject.to_cluster
+
+        expect(cluster).to be_an(Array)
+        expect { cluster << 'aa' }.to raise_error(RuntimeError, "can't modify frozen Array")
+      end
+
+      it 'is memoized until an element is added' do
+        expect(subject).to receive(:to_cluster_hash).once
+        3.times { subject.to_cluster }
+
+        subject << sp_sc
+
+        expect(subject).to receive(:to_cluster_hash).once
+        3.times { subject.to_cluster }
+      end
+    end
+
+    context 'when there are no routes' do
+      subject { DistanceMatrix::RouteCluster.new(origin: sp) }
+    end
+  end
+end
